f850913b7d3ae8b5ebc722b7f00d30b189a2ed6a
Wiktor Wójcik <wiktorwojcik112@gmail.com>
2022-11-26 17:45:51 +0100

    fix(index): Fix navigation bar not showing up and cards overlaying on small screens

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function toggleHamburger() {
   let menu = document.getElementById("hamburger_menu")
-console.log(menu.style.visibility)
-  if (menu.style.visibility === "hidden") {
+
+  if (menu.style.visibility === "hidden" || menu.style.visibility === "") {
     menu.style.visibility = "visible"
   } else {
     menu.style.visibility = "hidden"
